624ff1777259ca810766c61ba1cc551f9a61269b
Anderson Vieira <asvieira.br@gmail.com>
2024-01-28 02:32:45 -0300

    refactor(CATEGORY): make category description as unique constraint

--- src/modules/product-category/entities/product-category.entity.ts
+++ src/modules/product-category/entities/product-category.entity.ts
@@ -15,7 +15,7 @@ export class ProductCategoryEntity {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ name: 'description', nullable: false })
+  @Column({ name: 'description', nullable: false, unique: true })
   description: string;
 
   @OneToMany(() => ProductEntity, (product) => product.category)
