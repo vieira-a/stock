80b189ab3fa435a53eef9568a4df010dd64db1d3
Anderson Vieira <asvieira.br@gmail.com>
2024-01-28 00:27:38 -0300

    refactor(TYPE): standardize ProductTypeController responses

--- src/modules/product-type/controllers/product-type.controller.ts
+++ src/modules/product-type/controllers/product-type.controller.ts
@@ -3,13 +3,15 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
+  HttpStatus,
   NotFoundException,
   Param,
   ParseIntPipe,
   Patch,
   Post,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 
 import { CreateProductTypeDto, UpdateProductTypeDto } from '../dtos';
 import { ProductTypeService } from '../services';
@@ -19,55 +21,65 @@ export class ProductTypeController {
   constructor(private readonly service: ProductTypeService) {}
 
   @Post()
-  @HttpCode(201)
-  async create(@Body() productTypeData: CreateProductTypeDto) {
+  async create(
+    @Body() productTypeData: CreateProductTypeDto,
+    @Res() res: Response,
+  ): Promise<Response> {
     await this.service.create(productTypeData);
-    return 'Tipo de produto registrado com sucesso';
+    return res
+      .status(HttpStatus.CREATED)
+      .json({ message: 'Tipo de produto registrado com sucesso' });
   }
 
   @Get()
-  @HttpCode(200)
-  async readAll() {
-    return await this.service.readAll();
+  async readAll(@Res() res: Response): Promise<Response> {
+    const productTypes = await this.service.readAll();
+    return res.status(HttpStatus.OK).json({
+      data: productTypes,
+    });
   }
 
   @Get(':id')
-  @HttpCode(200)
-  @HttpCode(404)
-  async readById(@Param('id', ParseIntPipe) id: number) {
+  async readById(
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ): Promise<Response> {
     const productType = await this.service.readById(id);
 
     if (!productType) {
       throw new NotFoundException('Tipo de produto não encontrado');
     }
-    return productType;
+    return res.status(HttpStatus.OK).json({
+      data: productType,
+    });
   }
 
   @Patch(':id')
-  @HttpCode(200)
-  @HttpCode(404)
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() productTypeData: UpdateProductTypeDto,
-  ) {
+    @Res() res: Response,
+  ): Promise<Response> {
     const productType = await this.service.readById(id);
     if (!productType) {
       throw new NotFoundException('Tipo de produto não encontrado');
     }
     await this.service.update(id, productTypeData);
-    return 'Tipo de produto atualizado com sucesso';
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Tipo de produto atualizado com sucesso' });
   }
 
   @Delete(':id')
-  @HttpCode(200)
-  @HttpCode(404)
-  async delete(@Param('id', ParseIntPipe) id: number) {
+  async delete(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
     const productType = await this.service.readById(id);
 
     if (!productType) {
       throw new NotFoundException('Tipo de produto não encontrado');
     }
     await this.service.delete(id);
-    return 'Tipo de produto excluído com sucesso';
+    return res
+      .status(HttpStatus.OK)
+      .json({ message: 'Tipo de produto excluído com sucesso' });
   }
 }
